a5b4b26d17c7b512a92dbf8338c97c214cb73ed1
ghouse <gousaddin.md@gmail.com>
2017-12-20 14:50:50 +0530

    my learning program

---
+++ src/main/java/NewProject/Condition/IfProg.java
@@ -0,0 +1,18 @@
+package NewProject.Condition;
+
+public class IfProg 
+{
+			   public static void main(String args[])
+			  {
+		      int num=70;
+		      if( num < 100 )
+		     {
+			  /* This println statement will only execute,
+			   * if the above condition is true
+			   */
+			  System.out.println("number is less than 100");
+		      }
+		   }
+		}
+
+	
